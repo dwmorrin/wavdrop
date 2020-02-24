c1ae73d584db97f48ed70d347d219860db7b7e7c
David Morrin <dm187@nyu.edu>
2020-02-24 16:35:45 -0500

    feat: dropping random samples, printing times

--- wavdrop.py
+++ wavdrop.py
@@ -1,4 +1,5 @@
 """ randomly zero out samples in a .wav file """
+from random import sample
 import argparse
 import wave
 
@@ -6,6 +7,8 @@ parser = argparse.ArgumentParser(
         description="Randomly zero out samples in a .wav file")
 parser.add_argument('-o', '--output', help="output filename",
         dest='filename', default="bad.wav", action='store')
+parser.add_argument('-n', '--drops', help="number of dropped samples",
+        dest='ndrops', type=int, default=10, action='store')
 parser.add_argument('input_file')
 args = parser.parse_args()
 
@@ -14,6 +17,16 @@ wav_read = wave.open(args.input_file, 'rb')
 wav_params = wav_read.getparams()
 data = bytearray(wav_read.readframes(wav_params.nframes))
 wav_read.close()
+
+# TODO this printed 7.163, in Audacity was around 6.738
+def time_of_sample(sample_rate, sample):
+    return float(sample)/float(sample_rate)
+
+drops = sample(range(wav_params.nframes), args.ndrops)
+print("Dropping at the following times [seconds]:")
+for n in drops:
+    data[n] = 0
+    print("\t{0:.3f} s".format(time_of_sample(wav_params.framerate, n)))
 
 # write output file
 wav_write = wave.open(args.filename, 'wb')
