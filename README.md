34d34f974ceddb6f65e5da3ee489574d3697eb1f
David Morrin <dm187@nyu.edu>
2020-02-28 12:46:23 -0500

    feat: option to randomly drop left or right
    If -s or --stereo argument is given with a 2 channel file,
    the drop will be applied to left or right, chosen at random.

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # wavdrop
 
 Corrupt .wav files for the purpose of ear training.
+
 ```
-usage: wavdrop.py [-h] [-o FILENAME] [-n NDROPS] [-w DROP_WIDTH] input_file
+usage: wavdrop.py [-h] [-o FILENAME] [-n NDROPS] [-s] [-w DROP_WIDTH]
+                  input_file
 
 Randomly zero out samples in a .wav file
 
@@ -15,10 +17,9 @@ optional arguments:
                         output filename
   -n NDROPS, --drops NDROPS
                         number of dropped samples
+  -s, --stereo          apply drops to stereo channels separately
   -w DROP_WIDTH, --width DROP_WIDTH
                         width of dropped samples
 ```
-
-## wavgen
 
 To generate .wav files for testing, see [wavgen](https://github.com/dwmorrin/wavgen)
--- wavdrop.py
+++ wavdrop.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ randomly zero out samples in a .wav file """
-from random import sample
+from random import choice, sample
 import argparse
 import wave
 
@@ -10,6 +10,8 @@ parser.add_argument('-o', '--output', help="output filename",
         dest='filename', default="bad.wav")
 parser.add_argument('-n', '--drops', help="number of dropped samples",
         dest='ndrops', type=int, default=10)
+parser.add_argument('-s', '--stereo', help="apply drops to stereo channels separately",
+        action='store_true')
 parser.add_argument('-w', '--width', help="width of dropped samples",
         dest='drop_width', type=int, default=3)
 parser.add_argument('input_file')
@@ -29,13 +31,20 @@ drops = sample(range(wav_params.nframes - args.drop_width), args.ndrops)
 drops.sort()
 print("Dropping at the following times [seconds]:")
 FRAME_SIZE = wav_params.nchannels * wav_params.sampwidth
+dropped_channel = ""
 for n in drops:
     start_index = FRAME_SIZE * n
+    if args.stereo:
+        dropped_channel = choice(['L', 'R'])
     for i in range(args.drop_width):
         for j in range(wav_params.nchannels):
+            if args.stereo and dropped_channel == 'L' and j == 1:
+                continue
+            if args.stereo and dropped_channel == 'R' and j == 0:
+                continue
             for k in range(wav_params.sampwidth):
                 data[start_index + i*FRAME_SIZE + j*wav_params.sampwidth + k] = 0
-    print("\t{0:7.3f} s".format(time_of_sample(wav_params.framerate, n)))
+    print("\t{}{:7.3f} s".format(dropped_channel, time_of_sample(wav_params.framerate, n)))
 
 # write output file
 wav_write = wave.open(args.filename, 'wb')
